92e11dddc9006ddb10f4e820859c188bce8742ab
Brian Chen <brianntgal@gmail.com>
2024-08-12 02:46:55 -0400

    s

--- project5/settings.py
+++ project5/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cx)8-0kklp6c^k_lfv*o^ubi*&ht^aiwlr2=-&&3wxe)%hz1p_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
